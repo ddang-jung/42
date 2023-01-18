5a78c8531803905a8e1a8176e7225701a3f2df60
ddang_jung <hjk3376@naver.com>
2023-01-18 19:06:59 +0900

    20230118

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: hyeonjun <hyeonjun@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 13:59:44 by hyeonjun          #+#    #+#             */
-/*   Updated: 2023/01/14 14:52:36 by hyeonjun         ###   ########.fr       */
+/*   Updated: 2023/01/18 15:58:40 by hyeonjun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,27 @@
 
 # include "../get_next_line/get_next_line.h"
 # include "../libft/libft.h"
+# include "../mlx/mlx.h"
 
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef enum e_keycode
+{
+	KEY_W = 13,
+	KEY_A = 0,
+	KEY_S,
+	KEY_D,
+	KEY_ESC = 53,
+}	t_keycode;
+
+typedef enum e_size
+{
+	WIDTH = 50,
+	HEIGHT = 50,
+}
 typedef enum e_status
 {
 	ERROR = -1,
--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,7 +6,7 @@
 /*   By: hyeonjun <hyeonjun@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 13:58:57 by hyeonjun          #+#    #+#             */
-/*   Updated: 2023/01/17 17:21:12 by hyeonjun         ###   ########.fr       */
+/*   Updated: 2023/01/18 18:49:26 by hyeonjun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,7 @@ typedef struct s_copy
 	int		exit;
 }	t_copy;
 
-
-typedef struct s_input
+typedef struct s_board
 {
 	char	**map;
 	int		garo;
@@ -29,7 +28,33 @@ typedef struct s_input
 	int		coin;
 	int		exit;
 	int		status;
-}	t_input;
+}	t_board;
+
+typedef struct s_player
+{
+	int		x;
+	int		y;
+	int		status;
+	void	*img;
+}	t_player
+
+typedef struct s_exit
+{
+	int		x;
+	int		y;
+	int		status;
+	void	*img;
+	void	*open_img;
+}
+
+typedef struct s_game
+{
+	void	*mlx;
+	void	*win;
+	void	*tile;
+	void	*wall;
+	void	*collect;
+}	t_game;
 
 void	error_handler(char *error_message)
 {
@@ -37,18 +62,18 @@ void	error_handler(char *error_message)
 	exit(EXIT_FAILURE);
 }
 
-void	clear_input(t_input *input)
+void	clear_board(t_board *board)
 {
-	input->map = NULL;
-	input->garo = 0;
-	input->sero = 0;
-	input->player = 0;
-	input->coin = 0;
-	input->exit = 0;
-	input->status = 0;
+	board->map = NULL;
+	board->garo = 0;
+	board->sero = 0;
+	board->player = 0;
+	board->coin = 0;
+	board->exit = 0;
+	board->status = 0;
 }
 
-void	get_input(char *file, t_input *input)
+void	init_board(char *file, t_board *board)
 {
 	int		fd;
 	char	*line;
@@ -70,7 +95,7 @@ void	get_input(char *file, t_input *input)
 	}
 	if (one_line_map == NULL)
 		error_handler("Nothing in file?");
-	input->map = ft_split(one_line_map, '\n');
+	board->map = ft_split(one_line_map, '\n');
 }
 
 size_t	ft_strslen(char **strs)
@@ -83,46 +108,46 @@ size_t	ft_strslen(char **strs)
 	return (len);
 }
 
-void	component_check(char c, t_input *input)
+void	component_check(char c, t_board *board)
 {
 	if (c == '0' || c == '1' || c == 'P' || c == 'C' || c == 'E')
 	{
 		if (c == 'P')
-			input->player++;
+			board->player++;
 		if (c == 'C')
-			input->coin++;
+			board->coin++;
 		if (c == 'E')
-			input->exit++;
+			board->exit++;
 	}
 	else
 		error_handler("Map Contains Character Other than 0, 1, P, C, E.");
 }
 
-void	map_check(t_input *input)
+void	map_check(t_board *board)
 {
 	int		str_index;
 	int		chr_index;
 
-	input->garo = ft_strlen(*(input->map));
-	input->sero = ft_strslen(input->map);
-	if (input->garo == input->sero)
+	board->garo = ft_strlen(*(board->map));
+	board->sero = ft_strslen(board->map);
+	if (board->garo == board->sero)
 		error_handler("Map is Square.");
 	str_index = -1;
-	while (input->map[++str_index] != NULL)
+	while (board->map[++str_index] != NULL)
 	{
 		chr_index = -1;
-		while (input->map[str_index][++chr_index] != '\0')
+		while (board->map[str_index][++chr_index] != '\0')
 		{
-			component_check(input->map[str_index][chr_index], input);
-			if (str_index == 0 || str_index == (input->sero - 1) \
-			|| chr_index == 0 || chr_index == (input->garo - 1))
-				if (input->map[str_index][chr_index] != '1')
+			component_check(board->map[str_index][chr_index], board);
+			if (str_index == 0 || str_index == (board->sero - 1) \
+			|| chr_index == 0 || chr_index == (board->garo - 1))
+				if (board->map[str_index][chr_index] != '1')
 					error_handler("There is a Hole in the Wall.");
 		}
-		if (input->garo != chr_index)
+		if (board->garo != chr_index)
 			error_handler("Map is Not Rectangular.");
 	}
-	if (input->player != 1 || input->coin < 1 || input->exit != 1)
+	if (board->player != 1 || board->coin < 1 || board->exit != 1)
 		error_handler("Component Count Error.");
 }
 
@@ -146,58 +171,56 @@ void	location_finder(char **map, char c, int *loc)
 	}
 }
 
-void	do_dfs(t_input *input, t_copy *copy, int x, int y)
+void	do_dfs(t_board *board, t_copy *copy, int x, int y)
 {
 	int			index;
 	const int	dx[4] = {1, -1, 0, 0};
 	const int	dy[4] = {0, 0, 1, -1};
 
-	printf("%d %d %c %c\n", x, y, input->map[y][x], copy->map[y][x]);
 	if (copy->map[y][x] == '1')
 		return ;
 	else
 		copy->map[y][x] = '1';
-	if (input->map[y][x] == '1')
+	if (board->map[y][x] == '1')
 		return ;
-	else if (input->map[y][x] == 'C')
+	else if (board->map[y][x] == 'C')
 		copy->coin -= 1;
-	else if (input->map[y][x] == 'E')
+	else if (board->map[y][x] == 'E')
 		copy->exit -= 1;
-	printf("coin: %d exit: %d\n", copy->coin, copy->exit);
 	if (copy->coin == 0 && copy->exit == 0)
-		input->status = 1;
+		board->status = 1;
 	index = -1;
 	while (++index < 4)
-		if (x + dx[index] < input->garo && y + dy[index] < input->sero)
-			do_dfs(input, copy, x + dx[index], y + dy[index]);
+		if (x + dx[index] < board->garo && y + dy[index] < board->sero)
+			do_dfs(board, copy, x + dx[index], y + dy[index]);
 }
 
-void	path_check(t_input *input)
+void	path_check(t_board *board)
 {
 	t_copy	copy;
 	int		loc[2];
 	int		index;
 
-	copy.map = (char **)malloc(sizeof(char *) * (input->sero + 1));
+	copy.map = (char **)malloc(sizeof(char *) * (board->sero + 1));
 	if (copy.map == NULL)
 		error_handler("Function Malloc Returned NULL.");
 	index = -1;
-	while (++index < input->sero)
-		copy.map[index] = ft_strdup(input->map[index]);
+	while (++index < board->sero)
+		copy.map[index] = ft_strdup(board->map[index]);
 	copy.map[index] = NULL;
-	copy.coin = input->coin;
-	copy.exit = input->exit;
-	location_finder(input->map, 'P', loc);
-	do_dfs(input, &copy, loc[0], loc[1]);
-	if (input->status == 0)
+	copy.coin = board->coin;
+	copy.exit = board->exit;
+	location_finder(board->map, 'P', loc);
+	do_dfs(board, &copy, loc[0], loc[1]);
+	if (board->status == 0)
 		error_handler("There is No Available Path in This Map...");
 	index = -1;
-	while (++index < input->sero)
+	while (++index < board->sero)
 		free(copy.map[index]);
 	free(copy.map);
 }
 
-void	valid_check(int argc, char **argv, t_input *input)
+void	valid_check(int argc, char **argv, t_board *board)
 {
 	if (argc != 2)
 		error_handler("Invalid Number of Arguments!");
@@ -205,18 +228,26 @@ void	valid_check(int argc, char **argv, t_input *input)
 		error_handler("You Put NULL Argument!");
 	if (ft_strnstr(argv[1], ".ber", ft_strlen(argv[1])) == NULL)
 		error_handler("You Should Put .ber File.");
-	clear_input(input);
-	get_input(argv[1], input);
-	map_check(input);
-	path_check(input);
+	clear_board(board);
+	init_board(argv[1], board);
+	map_check(board);
+	path_check(board);
+}
+
+void	init_game(t_board *board, t_game *game)
+{
+	game->mlx = mlx_init();
+	game->win = mlx_new_window(game->mlx, board->garo * WIDTH, board->sero * HEIGHT, "so_long");
 }
 
 int	main(int argc, char **argv)
 {
-	t_input	input;
+	t_board	board;
+	t_game	game;
 
-	valid_check(argc, argv, &input);
-	for (int i = 0; input.map[i] != NULL; i++)
-		printf("%d %s\n", i + 1, input.map[i]);
+	valid_check(argc, argv, &board);
+	for (int i = 0; board.map[i] != NULL; i++)
+		printf("%s\n", board.map[i]);
+	init_game(&board, &game);
 	return (0);
 }
