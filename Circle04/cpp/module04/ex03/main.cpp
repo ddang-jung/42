dc37e182f8867a0ec410a5d9a21c1dbf66a0f019
ddang-jung <hyeonjun@cx2r4s3.42seoul.kr>
2023-06-28 19:40:19 +0900

    [FEAT] add test in main

--- Circle04/cpp/module04/ex03/main.cpp
+++ Circle04/cpp/module04/ex03/main.cpp
@@ -2,29 +2,70 @@
 #include "Ice.hpp"
 #include "Cure.hpp"
 #include "Character.hpp"
+#include <iostream>
 
 int main()
 {
-	IMateriaSource* src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
 
-	ICharacter* me = new Character("me");
+		ICharacter* me = new Character("me");
 
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
 
-	ICharacter* bob = new Character("bob");
+		ICharacter* bob = new Character("bob");
 
-	me->use(0, *bob);
-	me->use(1, *bob);
+		me->use(0, *bob);
+		me->use(1, *bob);
 
-	delete bob;
-	delete me;
-	delete src;
+		delete bob;
+		delete me;
+		delete src;
+	}
+	std::cout << "===============================" << std::endl;
+	{
+		IMateriaSource* src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
 
+		ICharacter* me = new Character("me");
+
+		AMateria* tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		ICharacter* bob = new Character("bob");
+
+		me->use(0, *bob);
+		me->use(1, *bob);
+		me->use(2, *bob);
+		me->use(3, *bob);
+
+		me->unequip(3);
+
+		me->use(0, *bob);
+		me->use(1, *bob);
+		me->use(2, *bob);
+		me->use(3, *bob);
+
+		delete bob;
+		delete me;
+		delete src;
+	}
+	// system("leaks ex03_");
 	return 0;
 }
