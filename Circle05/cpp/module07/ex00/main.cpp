73fbc654fd7910706975c18441262e25ca8d167f
hyeonjun <hjk3376@gmail.com>
2023-08-17 18:38:19 +0900

    [FEAT] modified main to subject example

--- Circle05/cpp/module07/ex00/main.cpp
+++ Circle05/cpp/module07/ex00/main.cpp
@@ -1,23 +1,22 @@
 #include "whatever.hpp"
 #include <iostream>
 
-int	main(void) {
-	int		a = 1, b = 2;
-	double	x = 1.123, y = 1.234;
+int main( void ) {
+	int a = 2;
+	int b = 3;
 
-	std::cout << "a: " << a << ", b: " << b << std::endl;
-	swap(a, b);
-	std::cout << "a: " << a << ", b: " << b << std::endl;
-	std::cout << "min(a,b): " << min(a, b) << std::endl;
-	std::cout << "max(a,b): " << max(a, b) << std::endl;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-	std::cout << "==================" << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
 
-	std::cout << "x: " << x << ", y: " << y << std::endl;
-	swap(x, y);
-	std::cout << "x: " << x << ", y: " << y << std::endl;
-	std::cout << "min(x,y): " << min(x, y) << std::endl;
-	std::cout << "max(x,y): " << max(x, y) << std::endl;
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
-	return (0);
+	return 0;
 }
