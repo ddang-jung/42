f7aab36192426a08c1f534c4cc48542ac6a386ad
Hyeonjung Kim <hyeonjun@cx2r3s7.42seoul.kr>
2023-10-30 16:20:24 +0900

    [FEAT] throw error when argument is not 2

--- Circle05/cpp/module09/ex00/main.cpp
+++ Circle05/cpp/module09/ex00/main.cpp
@@ -1,20 +1,22 @@
 #include "BitcoinExchange.hpp"
 #include <iostream>
 
-int	main(int ac, char **av) {
+int main(int ac, char **av)
+{
 	try
 	{
-		// if (ac != 2)
-		// 	throw Exception::OpenFailException();
-		BitcoinExchange	exchange_center;
+		if (ac != 2)
+			throw Exception::OpenFailException();
+		BitcoinExchange exchange_center;
+
 		exchange_center._exchange(av[1]);
 		(void)ac;
 		(void)av;
 	}
-	catch(const std::exception& e)
+	catch (const std::exception &e)
 	{
 		std::cerr << e.what() << '\n';
 	}
-	
+
 	return (0);
 }
